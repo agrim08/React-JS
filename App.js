d376f15668ab55c84b91bf0b5e39967a9ce54d46
agrim08 <agrimgupta0805@gmail.com>
2024-09-28 19:19:27 +0530

    Learned parcel

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
-const heading1 = React.createElement("h1", { id: "title" }, "Heading 1");
+/* PARCEL:
+ *
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning Up
+ * Dev abd Production Build
+ * Super Fast build algo
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatble with Older version of browser
+ * HTTPS on dev
+ * Port Number
+ * Consisting hashing algo
+ * Zero Config
+ *
+ *
+ * Transitive Dependencies
+ *
+ *
+ * BrowerList
+ *
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1", { id: "title" }, "Parcel OP");
 const heading2 = React.createElement("h2", { id: "title" }, "Heading 2");
-const conatiner = React.createElement("div", { id: "conatiner" }, [
-  heading1,
-  heading2,
-]);
+const conatiner = React.createElement(
+  "div",
+  { id: "conatiner", hello: "world" },
+  [heading1, heading2]
+);
 //this is how we add multiple element using array
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
