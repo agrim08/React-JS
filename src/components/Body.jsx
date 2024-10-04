b8d441c096463cb157d09a28b05be6073a74865f
agrim08 <agrimgupta0805@gmail.com>
2024-10-04 23:39:49 +0530

    Search Feature completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,48 +1,76 @@
 import { useState, useEffect } from "react";
-import restaurantsList from "../Data/restrauntList";
 import RestaurantCard from "./RestaurantCard";
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
 
 const filterData = (searchText, restaurants) => {
+  console.log("Search text:", searchText.toLowerCase());
   const filteredData = restaurants?.filter((restaurant) =>
-    restaurant?.info?.name?.includes(searchText)
+    restaurant?.info?.name?.toLowerCase()?.includes(searchText.toLowerCase())
   );
   return filteredData;
 };
 
-// const myJSON = { a: "Ford", b: "BMW", c: "Fiat" };
+//* const myJSON = { a: "Ford", b: "BMW", c: "Fiat" };
 // const myArray = JSON.stringify(myJSON);
 // console.log(myArray);
 
 function Body() {
   //* searchText is a local state variable
   const [searchText, setSearchText] = useState(""); //* to create state variable
-  const [restaurants, setRestaurants] = useState(restaurantsList);
+  const [allRestaurants, setAllRestaurants] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     getRestaurants();
   }, []);
 
   async function getRestaurants() {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const jsonData = await data.json();
-    setRestaurants(
-      jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants
-    );
-    console.log(jsonData);
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      if (!data.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const jsonData = await data.json();
+      setAllRestaurants(
+        jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setFilteredRestaurants(
+        jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      console.log(jsonData);
+    } catch {
+      setError(error.message);
+    }
   }
-  // console.log(true);
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  //* console.log(true);
 
   function handleSearch(e) {
     e.preventDefault();
-    const data = filterData(searchText, restaurantsList);
-    setRestaurants(data);
+    const data = filterData(searchText, allRestaurants);
+    setFilteredRestaurants(data);
   }
-  // console.log(restaurants);
+  //* console.log(restaurants);
 
-  return (
+  //* early return
+  if (!allRestaurants) return null;
+
+  if (filteredRestaurants?.length === 0) return <h1>No restaurant found</h1>;
+
+  return allRestaurants?.length === 0 ? (
+    <>
+      <ShimmerSimpleGallery imageHeight={350} col={4} caption />
+      <ShimmerSimpleGallery card imageHeight={350} />
+      <ShimmerSimpleGallery card imageHeight={350} caption />
+    </>
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -58,9 +86,8 @@ function Body() {
           Search
         </button>
       </div>
-
       <div className="restraunt-lists">
-        {restaurants?.map((restaurant) => {
+        {filteredRestaurants?.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant?.info} key={restaurant?.info?.id} />
           );
@@ -69,4 +96,5 @@ function Body() {
     </>
   );
 }
+
 export default Body;
